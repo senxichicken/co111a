b27caac323905dfb081368b1117164cb0914dc87
senxichicken <gordon50512@gmail.com>
2022-10-16 15:55:38 +0800

    1016 04 done

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,24 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@2
+M=0
+@0
+D=M
+@21
+D;JEQ
+@1
+D=M
+@21
+D;JEQ
+@12
+D;JNE
+@0
+D=M
+@2
+M=M+D
+@1
+M=M-1
+@6
+0;JMP
